cba56beea219b5784707e41fd60a797b785a8ced
riban <riban@zynthian.org>
2025-01-09 16:40:42 +0000

    Fixes audio output routing.
    There is now bold press to show audio levels but it isn't working.
    Left code so it can be fixed in future.

--- zyngui/zynthian_gui_audio_out.py
+++ zyngui/zynthian_gui_audio_out.py
@@ -140,10 +140,25 @@ class zynthian_gui_audio_out(zynthian_gui_selector_info):
 
     def select_action(self, i, t='S'):
         if self.list_data[i][0] == 'record':
-            self.zyngui.state_manager.audio_recorder.toggle_arm(self.chain.mixer_chan)
-        else:
-            self.chain.toggle_audio_out(self.list_data[i][0])
-        self.fill_list()
+            if t == 'S':
+                self.zyngui.state_manager.audio_recorder.toggle_arm(self.chain.mixer_chan)
+                self.fill_list()
+        elif t == 'S':
+            self.chain.toggle_audio_in(self.list_data[i][0])
+            self.fill_list()
+        elif t == "B":
+            self.zyngui.state_manager.start_busy("alsa_output")
+            zctrls = self.zyngui.state_manager.alsa_mixer_processor.engine.get_controllers_dict()
+            ctrl_list = []
+            for symbol, zctrl in zctrls.items():
+                try:
+                    if zctrl.graph_path[4] in self.list_data[i][1]:
+                        ctrl_list.append(symbol)
+                except:
+                    pass
+            self.zyngui.state_manager.end_busy("alsa_output")
+            if ctrl_list:
+                self.zyngui.show_screen("alsa_mixer", params=ctrl_list)
 
     def set_select_path(self):
         self.select_path.set("Send Audio to ...")
